3ef282d31aeb80ac6e2f1087e647b5ffc877365b
XiaoWu-5759 <1981293730@qq.com>
2019-07-11 17:34:20 +0800

    add

--- xiaowu-security/xiaowu-security-core/src/main/java/com/xiaowu/security/core/properties/QQProperties.java
+++ xiaowu-security/xiaowu-security-core/src/main/java/com/xiaowu/security/core/properties/QQProperties.java
@@ -2,16 +2,17 @@ package com.xiaowu.security.core.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.social.SocialProperties;
 
 @Getter
 @Setter
-public class QQProperties {
+public class QQProperties extends SocialProperties {
 
     // application id
-    private String appId;
+//    private String appId;
 
     // application secret
-    private String appSecret;
+//    private String appSecret;
 
     private String providerId = "qq";
 }
