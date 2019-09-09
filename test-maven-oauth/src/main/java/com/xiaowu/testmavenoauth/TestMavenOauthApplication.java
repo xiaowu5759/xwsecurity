fcd1a1bbdf7b6b1fd890809964f63e941c13ac65
XiaoWu-5759 <1981293730@qq.com>
2019-09-09 09:01:13 +0800

    add

---
+++ test-maven-oauth/src/main/java/com/xiaowu/testmavenoauth/TestMavenOauthApplication.java
@@ -0,0 +1,13 @@
+package com.xiaowu.testmavenoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMavenOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMavenOauthApplication.class, args);
+	}
+
+}
